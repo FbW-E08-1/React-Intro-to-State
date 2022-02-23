1370aba5643ef3da2165108b656050eb214ad473
jsk900 <jsk900@yahoo.co.uk>
2022-02-23 15:06:43 +0100

    E08 React Intro to State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Counter from './Counter';
+import Form from './Form';
 
 const App = () => {
+  const [name, setName] = useState(true);
+  const [counter, setCounter] = useState(5);
+
+  const reset = () => {
+    setCounter(0);
+  };
+
+  console.log('rendered');
+
+  const change = () => {
+    setName(!name);
+  };
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <h1>Welcome to React</h1>
+      <button onClick={change}>Change</button>
+      <br />
+      {name ? 'barry' : 'sally'}
+
+      <Form counter={counter} />
+      <Counter reset={reset} counter={counter} setCounter={setCounter} />
     </main>
   );
-}
+};
 
 export default App;
-
